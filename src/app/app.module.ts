86c08f248dfc32a6c033615625ed2c075d1c091d
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-07-11 11:23:53 +0530

    UI created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ColumnComponent } from './column/column.component';
+import { BodyComponent } from './body/body.component';
+import { CardComponent } from './card/card.component';
+import { AddColumnComponent } from './add-column/add-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ColumnComponent,
+    BodyComponent,
+    CardComponent,
+    AddColumnComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/card.ts
@@ -0,0 +1,5 @@
+export interface card{
+  Title:string;
+  Desciption:string;
+  Assignee:string;
+};
