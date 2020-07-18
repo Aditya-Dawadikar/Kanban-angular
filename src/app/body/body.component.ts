862e319fc05bbd7eca28fac0def13a9e28ec78b7
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-07-18 16:52:51 +0530

    delete all cards when column delete is not fixed but can be handled when DB is connected

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {COLUMNS} from '../shared/mock-column';
+import {CARDS} from '../shared/mock-card';
 
 @Component({
   selector: 'app-body',
@@ -12,6 +13,7 @@ export class BodyComponent implements OnInit {
   @Output() columnEmitter= new EventEmitter();
 
   Columns=COLUMNS;
+  Cards=CARDS;
 
   constructor() {
   }
@@ -22,5 +24,10 @@ export class BodyComponent implements OnInit {
   deleteColumnFromArray(column){
     let index=this.Columns.indexOf(column);
     this.Columns.splice(index,1);
+
+    let newArray=this.Cards.filter(function(card){
+      return card.status!==column.columnType;
+    });
+    this.Cards=newArray;
   }
 }
--- src/app/shared/mock-card.ts
+++ src/app/shared/mock-card.ts
@@ -1,7 +1,12 @@
 import {Card} from './card';
 
-/*
+
 export const CARDS:Card[]=[
+  {
+    columnName:"Todo",
+    task:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status:"incomplete"
+  },
   {
     columnName:"Todo",
     task:"get coke",
@@ -24,6 +29,6 @@ export const CARDS:Card[]=[
     status:"issue"
   }
 ];
-*/
 
-export const CARDS:Card[]=[];
+
+//export const CARDS:Card[]=[];
